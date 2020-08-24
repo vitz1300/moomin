2ce4ea01befbda40662206ae23eee645d8cc87e2
vitz1300 <vitz1300cat@gmail.com>
2020-08-24 19:38:32 +0900

    条件分岐の追加、数値判追加

--- main.js
+++ main.js
@@ -4,27 +4,31 @@ const input2=document.getElementById('buzzvalue');
 const input1=document.getElementById('fizzvalue');
 button.addEventListener('click',()=>{  
   const boxNum =document.getElementById('box');
-  const fizz=input1.value;
-  const buzz=input2.value;
-
+  const fizz=Number(input1.value);
+  const buzz=Number(input2.value);
+  
     boxNum.innerHTML="";
-    if(Number(fizz)&&Number(buzz)){
-          for(let i = 1; i <= 99; i++){
-                  const p=document.createElement('p');
-                  if(i%fizz===0&&i%buzz===0){
-                    p.textContent=`FizzBuzz${i}`;
-                    boxNum.appendChild(p);  
-                  }else if(i%fizz===0){
-                    p.textContent=`Fizz${i}`;
-                    boxNum.appendChild(p);  
-                  }else if(i%buzz===0){
-                    p.textContent=`Buzz${i}`;
-                    boxNum.appendChild(p);  
-                  }
-                };
-    }else{
+    if(fizz===0&&buzz===0){
       boxNum.textContent=`整数値を入力して下さい`;
     };
+
+      if(Number.isInteger(fizz)&&Number.isInteger(buzz)){  
+        for(let i = 1; i <= 99; i++){
+          const p=document.createElement('p');
+            if(i%fizz===0&&i%buzz===0){
+              p.textContent=`FizzBuzz${i}`;
+              boxNum.appendChild(p);  
+            }else if(i%fizz===0){
+              p.textContent=`Fizz${i}`;
+              boxNum.appendChild(p);  
+            }else if(i%buzz===0){
+              p.textContent=`Buzz${i}`;
+              boxNum.appendChild(p);  
+            }
+          };
+      }else{
+      boxNum.textContent=`整数値を入力して下さい`;
+  };
 });
   
 
