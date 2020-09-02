e3f628738353a46cb01a9425c5ad58fa410f92e4
vitz1300 <vitz1300cat@gmail.com>
2020-09-03 01:08:05 +0900

    関数でまとめて、オブジェクト作成

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 'use strict'
-const addButton = document.getElementById('addbutton');
-
+const addButton = document.getElementById('addbutton');//ボタン取得
 addButton.addEventListener('click', () => {
   const table = document.getElementById('myTable');//テーブル取得
   const newRow = table.insertRow(-1);//行の末に行を追加
   const cellA = newRow.insertCell(-1);//セルの追加
   const cellB = newRow.insertCell(-1);//セルの追加
-  const cellC = newRow.insertCell(-1);
-  const cellD = newRow.insertCell(-1);
+  const cellC = newRow.insertCell(-1);//セルの追加
+  const cellD = newRow.insertCell(-1);//セルの追加
   
-  const countUp = () =>{
+  const countUp = () =>{  //ID番号
     const row = myTable.rows.length -1;
     cellA.innerHTML = row;
   };
@@ -23,7 +22,7 @@ addButton.addEventListener('click', () => {
   const work = () =>{//作業中ボタンの関数
     cellC.innerHTML = '<input type="button" value="作業中"> '
   }; 
-  const erase = () => {
+  const erase = () => {//削除ボタンの関数
     cellD.innerHTML = '<input type="button" value="削除"> '
   };
   const todos = [];
