d369c4710a7ba7f2bed5d0d083cfe3a54564ac42
vitz1300 <vitz1300cat@gmail.com>
2020-09-09 12:46:36 +0900

    課題①-4オブジェクト修正

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 'use strict'
 const main =`==========================\n現在持っているのタスク一覧\n==========================`;
-const tasks = {
-  content: ['机を片付ける、','牛乳を買う、','散歩する、'],
-  genre: ['掃除','買い物','運動'],
-}
+const tasks = [
+  {content: '机を片付ける、', genre:'掃除'},
+  {content: '牛乳を買う、', genre:'買い物'},
+  {content: '散歩する', genre:'運動'},
+];
 
   function show(){
       console.log(main);
-      let id = Object.keys(tasks.content);
+      let id = Object.keys(tasks);
+      const contents = tasks.map(item => item.content);
+      const genres = tasks.map(item => item.genre);
       for(let i = 0; i < id.length; i++){
-        console.log(id[i]+':[内容]'+tasks.content[i]+'[ジャンル]'+tasks.genre[i]);  
+        console.log(id[i]+':[内容]'+contents[i]+'[ジャンル]'+genres[i]);  
       }
       };
   show();
@@ -18,8 +21,7 @@ const tasks = {
 function add(){
   const a = prompt('タスクを入力してください');
   const b = prompt('タスクを入力してください');
-  tasks.content.push(a);
-  tasks.genre.push(b);
+  tasks.push({content:a,genre:b});
   show();
   alert('新しいタスクを追加しました');
   prompt('「確認、追加、削除、終了」の4つのいずれかを入力してください');
