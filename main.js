176523e6c5ce0dc4a27072565813c633fc6909f6
vitz1300 <vitz1300cat@gmail.com>
2020-09-28 17:30:22 +0900

    課題③-2修正

--- main.js
+++ main.js
@@ -6,38 +6,36 @@ function clear(){
   const clearText = document.getElementById('comment');
   clearText.value = '';
 }
-console.log(todos);
 
 addButton.addEventListener('click', () => {
   let task = {comment: document.getElementById('comment').value,status: '作業中',}    
   todos.push(task);
   show();
   clear();
+  console.log(todos);
 })
 
 function show(){
-  const key = Object.keys(todos);
     table.innerHTML = "";
-    for(let i = 0; i < key.length; i++){ 
+    todos.forEach(function(value,index){
       const newRow = table.insertRow(-1);
       const cellId = newRow.insertCell(-1);
       const cellComment = newRow.insertCell(-1);
       const cellWork = newRow.insertCell(-1);
       const cellFinishButton = newRow.insertCell(-1);
-      const eraceBtn = document.createElement('button');
+      const createDeleteBtn = document.createElement('button');
       const statsBtn = document.createElement('button');
-  
-      cellId.textContent = key[i];
-      cellComment.textContent = todos[i].comment; 
-        statsBtn.textContent = todos[i].status;
-        cellWork.appendChild(statsBtn);
-        eraceBtn.textContent = '削除';
-        cellFinishButton.appendChild(eraceBtn)
-
-      eraceBtn.addEventListener('click',()=>{
-        todos.splice(i,1);
+      cellId.textContent = index;
+      cellComment.textContent = value.comment; 
+      statsBtn.textContent = value.status;
+      cellWork.appendChild(statsBtn);
+      createDeleteBtn.textContent = '削除';
+      cellFinishButton.appendChild(createDeleteBtn) 
+      
+      createDeleteBtn.addEventListener('click', () => {
+        todos.splice(index,1);
         console.log(todos);
         return show();
       })
-    }
-}
+    })
+  }
